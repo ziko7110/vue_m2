90f736f9c29601106fb80c734bd87c4a04c49c9c
ziko7110 <ziko7110@gmail.com>
2021-03-29 13:03:16 +0900

    run watch

---
+++ resources/js/home.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './components/HelloWorld'
+
+new Vue({
+  el: '#app', // 「el」はアプリケーションを紐付ける要素のセレクタです
+  components: {
+    app: App //使用するコンポーネントの名称と使うコンポーネントを指定します（app:名称, App(./components/HelloWorld)使うコンポーネント）を指定します
+  }
+})
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,13 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js');
+mix.js('resources/js/home.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css', {
+        implementation: require('dart-sass')
+    });
+
+mix.browserSync({
+    files: [
+        "public/**/*.*"
+    ],
+    proxy: "http://localhost:8000"
+});
