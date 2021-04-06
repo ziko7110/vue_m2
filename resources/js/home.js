78020ef66feb2f86ce908b5ec62181ee35ee46f3
ziko7110 <ziko7110@gmail.com>
2021-04-06 20:49:04 +0900

    フォルダ再生

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
-import App from './components/Home'
-import VueSocialSharing from 'vue-social-sharing';
+import App from './components/page/Home'
+import VueSocialSharing from 'vue-social-sharing'
 
 Vue.use(VueSocialSharing);
-
 new Vue({
-  el: '#app', // 「el」はアプリケーションを紐付ける要素のセレクタです
+  el: '#app',
   components: {
-    app: App //使用するコンポーネントの名称と使うコンポーネントを指定します（app:名称, App(./components/HelloWorld)使うコンポーネント）を指定します
+    app: App
   }
 })
