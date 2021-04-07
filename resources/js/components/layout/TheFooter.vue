5f77cef226dfd4b2a5b7f8db5a937c29f551b88e
ziko7110 <ziko7110@gmail.com>
2021-04-07 23:17:30 +0900

    header,footer追加

---
+++ resources/js/components/layout/TheFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer>
+    <div class="container text-center">
+      <small>© 2020 4Answers-quiz. All rights reserved.</small>
+    </div>
+  </footer>
+</template>
---
+++ resources/js/components/layout/TheHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <div class="container text-center">
+      <h1>4 Answers Quiz 4択クイズ&クイズ徹底解説</h1>
+      <a href="/">
+        <img class="header-nav__logo" src="/public/images/header-logo.png" />
+      </a>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand" id="header-nav__home" href="/">ホーム</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto"></ul>
+
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a id="header-nav__login" href="/login">ログイン</a>
+            </li>
+            <li class="nav-item">
+              <a id="header-nav__register" href="/register">ユーザー登録</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
